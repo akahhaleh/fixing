3dda60258882776b9227bad9d1a60cb4147a2a70
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-20 23:35:45 -0800

    [sign-up.handlebars]

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,7 +1,12 @@
 //var housingData = require('../housing.json');
 
 exports.view = function(req, res){
-	//console.log(housingData);
 
-	res.render('sign-up');
+	var city = (typeof req.query.city !=='undefined')?req.query.city:" ";
+	var numOfBeds = (typeof req.query.bed !=='undefined')?req.query.bed:0;
+	var numOfBaths = (typeof req.query.bath !=='undefined')?req.query.bath:0;
+	var numOfOccupants  = (typeof req.query.occupants !=='undefined')?req.query.occupants:0;
+	console.log("We logged: city= "+city+" #beds= "+numOfBeds+" #baths= "+numOfBaths+" #occupants= "+numOfOccupants);
+
+	res.render('sign-up', {city, numOfBeds, numOfBaths, numOfOccupants});
 };
