32f3f275eaa2822cff1f060a025c4817eadd57fa
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-21 00:11:33 -0800

    linking from listyourspace to signup

--- public/js/listYourSpace.js
+++ public/js/listYourSpace.js
@@ -37,8 +37,8 @@ function onsubmitform(e){
 	} else {
 		var listingProcess = true;
 		localStorage.setItem('listingProcess', listingProcess)
-		document.entireHomeForm.action ="/";
-		document.privateRoomForm.action ="/";
+		document.entireHomeForm.action ="/sign-up";
+		document.privateRoomForm.action ="/sign-up";
 
 	}	
 }
