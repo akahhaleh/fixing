db63a626bb93e4406a8a383c4934edced9c15659
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-21 00:41:30 -0800

    trying to pass variables after parsing them as a text object in sign-up.js

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,10 +1,15 @@
 exports.view = function(req, res){
 
-	var city = req.query.city;
-	var numOfBeds = req.query.bed;
-	var numOfBaths = req.query.bath;
-	var numOfOccupants  = req.query.occupants;
-	//console.log("We logged: city= "+city+" #beds= "+numOfBeds+" #baths= "+numOfBaths+" #occupants= "+numOfOccupants);
+	var city = (typeof req.query.city !=='undefined')?req.query.city:" ";
+	var numOfBeds = (typeof req.query.bed !=='undefined')?req.query.bed:0;
+	var numOfBaths = (typeof req.query.bath !=='undefined')?req.query.bath:0;
+	var numOfOccupants  = (typeof req.query.occupants !=='undefined')?req.query.occupants:0;
+	console.log("We logged: city= "+city+" #beds= "+numOfBeds+" #baths= "+numOfBaths+" #occupants= "+numOfOccupants);
 
-	res.render('sign-up', {city, numOfBeds, numOfBaths, numOfOccupants});
+	var text = '{"'+city+'","'+numOfBeds+'","'+numOfBaths+'","'+numOfOccupants+'"}';
+
+	console.log("text object includes:", text);
+	var localVariables = JSON.parse(text);
+
+	res.render('sign-up', localVariables);
 };
